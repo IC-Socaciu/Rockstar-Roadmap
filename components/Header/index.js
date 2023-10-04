a9449f1a8281a79b8bf7dc0d38d705dcbc518304
ICS <ioana@IoanaCrtinasMBP.fritz.box>
2023-10-04 14:52:06 +0200

    add background color to test repo

--- components/Header/index.js
+++ components/Header/index.js
@@ -2,9 +2,20 @@ import { styled } from "styled-components";
 
 export default function Header() {
   return (
+    <>
     <StyledHeader>
-      <h1>Art Gallery</h1>
+      <h1>Rockstar Roadmap</h1>
+<Navbar>
+  <ul>
+    <li>LINK 1</li>
+    <li>LINK 2</li>
+    <li>LINK 3</li>
+    <li>LINK 4</li>
+    <li>LINK 5</li>
+  </ul>
+</Navbar>
     </StyledHeader>
+    </>
   );
 }
 
@@ -12,8 +23,25 @@ const StyledHeader = styled.header`
   height: var(--header-height);
   position: fixed;
   text-align: center;
+  align-items: center;
   top: 0;
   width: 100%;
   padding: 1rem;
-  background-color: white;
+  background-color: #36013f;
+`;
+const Navbar = styled.nav`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  ul {
+    display: flex;
+    flex-direction: row;
+    list-style: none;
+    margin: 0;
+    padding: 0;
+    color: white;
+    li {
+      margin: 0 1rem;
+    }
+  }
 `;
--- styles.js
+++ styles.js
@@ -14,5 +14,6 @@ export default createGlobalStyle`
   body {
     font-family: system-ui;
     margin: 0;
+    background: #36013f;
   }
 `;
