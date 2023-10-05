66df34dfe597c3af04435b485ef8dec047985178
ICS <ioana@IoanaCrtinasMBP.fritz.box>
2023-10-05 15:13:15 +0200

    3 pages linking to and from are now working as well as hide and show info on Rider updated still not final

--- components/Rider/index.js
+++ components/Rider/index.js
@@ -156,22 +156,37 @@ const StyledRiderBody = styled.p`
 const MyApp = () => {
   const [showTechnical, setShowTechnical] = useState(true);
   const [showSound, setShowSound] = useState(true);
+  const [showEquipment, setShowEquipment] = useState(true);
+const [showArrival, setShowArrival] = useState(true);
+const [showFood, setShowFood] = useState(true);
+const [showBackstage, setShowBackstage] = useState(true);
+const [showSleep, setShowSleep] = useState(true);
+const [showMerch, setShowMerch] = useState(true);
+const [showMiscellaneous, setShowMiscellaneous] = useState(true);
+const [showPromo, setShowPromo] = useState(true);
 
   return (
     <>
-      <section>
+      <HiddenText>
         {showTechnical ? (
-          <p>
+          <HiddenParagraph>
             THE MINIMAL SET UP for small venues, up to 150 people (microphones
             are listed in order of preference): kick, snare, overhead, bass,
-            guitars, vocals.
-          </p>
+            guitars, vocals. Include your line up and equipment list.
+            n the first part of your tech rider, it is good practice to introduce the members of the band, mention their names and which instruments they will be playing. The idea is to give the venue a quick overview of how many people they are expecting and the kind of instrumentation. This can be done as a simple list but it can also be presented with a well organized table that highlights the band members, their instruments or for example, whether or not they are going to use an amplifier or a DI (Direct Injection) box … venues and engineers will be very appreciative if you do this!
+            In a live show setting, it’s fundamental to familiarize yourself with some technical terminology. One of the most common terms used is “backline”. The backline refers to all of that equipment that is going to be required on stage for the artist(s) to perform. If possible, try to include in your tech rider a list of all the songs you are going to perform. There are a few reasons to do so:
+
+It gives the venue and the engineers an idea of how long the show is going to be
+It provides a cue sheet to understand the different sections of the concert
+It is useful for lighting design
+It can be used by the venue for your royalty allocation (artists should receive royalty payments even when they perform by companies like ASCAP or BMI if their songs are registered)
+          </HiddenParagraph>
         ) : null}
         <button onClick={() => setShowTechnical(!showTechnical)}>
           Technical rider
         </button>
-      </section>
-      <section>
+      </HiddenText>
+      <HiddenText>
         {showSound ? (
           <p>
             We prefer an overall natural sound with minimal processing and
@@ -186,9 +201,115 @@ const MyApp = () => {
           </p>
         ) : null}
         <button onClick={() => setShowSound(!showSound)}>Sounds & Lighting</button>
+      </HiddenText>
+      <section>
+        {showEquipment ? (
+          <p>
+            The drummer always plays his own drumset and will not share his drums
+        with the other bands, except. Festivals. Apart from that we might share
+        one of our guitar cabinets (4x12“), but this needs to be discussed well
+        before the show. We will need an absolute minimum of 45 minutes
+        soundcheck, 60 to 90 minutes including the setup would be perfect.
+          </p>
+        ) : null}
+        <button onClick={() => setShowEquipment(!showEquipment)}>
+         Equipment and soundcheck
+        </button>
+      </section>
+      <section>
+        {showArrival ? (
+          <p>
+             Please provide detailed directions in x language or english language
+        two weeks before the date of the show with the complete adress of the
+        venue and a telephone contact. We need a safe parking space fort he van
+        close tot he venue. Please provie arrival, soundcheck and doors-open and
+        on-stage times. The promoter or antoher person should be at the venue at
+        arrival time.
+          </p>
+        ) : null}
+        <button onClick={() => setShowArrival(!showArrival)}>
+         Arriving to the venue
+        </button>
+      </section>
+      <section>
+        {showFood ? (
+          <p>
+        Hot meals and or normal meals or 12 € for each bandmember for buy-out, minimum 2 bottles of cooled non-carbonated water per person for the
+        stage and backstage, minimum 12 liter of other cooled drinks (no alcohol!)
+          </p>
+        ) : null}
+        <button onClick={() => setShowFood(!showFood)}>
+Food & Drinks        </button>
+      </section>
+      <section>
+        {showBackstage ? (
+          <p>
+        A sperate room for the band to get some rest before and after the show.
+        It would be great to have sofas, tables, chairs, a mirror and sink.
+          </p>
+        ) : null}
+        <button onClick={() => setShowBackstage(!showBackstage)}>
+Backstage        </button>
+      </section>
+      <section>
+        {showSleep ? (
+          <p>
+        A warm, clean, dark and quiet accomodation for all persons with
+        mattresses, bath room, shower and mirror. A safe parking place near the
+        sleeping accomodation sould be available. (Just for concerts further
+        than 200km)
+          </p>
+        ) : null}
+        <button onClick={() => setShowSleep(!showSleep)}>
+         Accomodation
+        </button>
+      </section>
+      <section>
+        {showMerch ? (
+          <p>
+                A table of approx. 1.5m lenght and a small light or some candles for our
+        merchandising in the same room as the show, if possible.
+          </p>
+        ) : null}
+        <button onClick={() => setShowMerch(!showMerch)}>
+Merchandise        </button>
+      </section>
+      <section>
+        {showMiscellaneous ? (
+          <p>
+         A guest list of 10 persons, meet & greet, backdrops, stage risers
+          </p>
+        ) : null}
+        <button onClick={() => setShowMiscellaneous(!showMiscellaneous)}>
+         Miscellaneous
+        </button>
+      </section>
+      <section>
+        {showPromo ? (
+          <p>
+There’s a large amount of promo material we can send to you for
+        advertising purposes. Don’t hestitate to write, if you need posters of
+        flyer and promo templates, logos or pictures in various file formats.
+        Just get in contact with us. There’s no corporate identity in our
+        advertising, but a corporate feeling for what the band is. We appreciate
+        good design, whether it’s professional or just made from the heart.          </p>
+        ) : null}
+        <button onClick={() => showPromo(!setShowPromo)}>
+Advertising and promotion        </button>
       </section>
     </>
   );
 };
 
 export { MyApp };
+
+const HiddenText = styled.div`
+border: solid 1px;
+color: white;
+border-radius: 5px;
+margin-bottom: 5px;
+`;
+
+const HiddenParagraph = styled.p`
+  color: white;
+`;
--- pages/art-pieces/index.js
+++ pages/art-pieces/index.js
@@ -1,5 +1,5 @@
 import ArtPieces from "../../components/ArtPieces";
-
+import { styled } from "styled-components";
 
 export default function artPiecesPage({
   data,
@@ -7,13 +7,12 @@ export default function artPiecesPage({
   artPiecesInfo,
 }) {
   return (
-    <>
-      <ArtPieces
-        data={data}
-        artPiecesInfo={artPiecesInfo}
-        handleToggleFavorite={handleToggleFavorite}
-      />
-      <p>Art Pieces Page TBD</p>
-    </>
+    
+      <ArtPageTest>This is redirected here to the Art Pieces Page</ArtPageTest>
+    
   );
 }
+const ArtPageTest = styled.h3`
+color: white;
+margin-top: 350px;
+`;
