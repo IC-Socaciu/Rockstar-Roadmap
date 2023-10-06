55bd13ca97cc9e21379fe4692dbb0b2fc0219a71
ICS <ioana@IoanaCrtinasMBP.fritz.box>
2023-10-06 16:24:46 +0200

    grid added to main page still unsure of design

--- components/AP/index.js
+++ components/AP/index.js
@@ -6,6 +6,34 @@ import ImageWithButton from "../ImageWithButton";
 
 export default function MainPage(){
   return (
-  <h1>TEST MAIN PAGE</h1>
-)}
+ 
+        <CardGrid>
+        {[...Array(6)].map((_, index) => (
+          <Card key={index}>
+            <CardText>Card {index + 1}</CardText>
+          </Card>
+        ))}
+      </CardGrid>
+    );
+  };
+  
 
+  const CardGrid = styled.div`
+  display: grid;
+  grid-template-columns: repeat(3, 1fr);
+  gap: 1rem;
+`;
+
+const Card = styled.div`
+  position: relative;
+  width: 100%;
+  padding-top: 100%;
+  background-color: #f5f5f5;
+`;
+
+const CardText = styled.div`
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+`;
